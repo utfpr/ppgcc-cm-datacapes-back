4fd121730a74a08724c9b424584e16222cb1f21e
als-v <alisson.v3@hotmail.com>
2022-04-25 21:59:03 -0300

    refactor: 💡 Change the lib to parser the xml file

--- src/services/parser/parser.ts
+++ src/services/parser/parser.ts
@@ -1,16 +1,24 @@
 import fs from 'fs';
-import { parseString, Builder } from "xml2js";
+import { XMLParser } from "fast-xml-parser"
+
+let options = {
+    ignoreAttributes : false,
+    ignoreNameSpace : false,
+    allowBooleanAttributes : true,
+    parseNodeValue : true,
+    parseAttributeValue : false,
+    trimValues: true,
+};
 
 export const executeFile = function(filePath: string) {
-
+    
     // abrir o arquivo
     let file = fs.readFileSync(filePath, {encoding:'utf8'});
-    let author_name = null;
-
-    // transformar em JSON
-    parseString(file, { explicitArray: false }, function(error, result) {
-        author_name = result['CURRICULO-VITAE']['DADOS-GERAIS']['$']['NOME-COMPLETO'];
-    });
-
-    return author_name;
+    
+    // cria o parser xml com as opções
+    const parser = new XMLParser(options)
+    const result = parser.parse(file)
+    
+    // retorna o nome do autor
+    return result['CURRICULO-VITAE']['DADOS-GERAIS']['@_NOME-COMPLETO'];
 };
