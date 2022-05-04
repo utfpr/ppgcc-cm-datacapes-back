d907a28f7aa6ff275f66c23e7c57f22468d62b4c
als-v <alisson.v3@hotmail.com>
2022-05-04 17:01:05 -0300

    feat: 🎸 Saving UserEntity in database

--- src/services/author/author.ts
+++ src/services/author/author.ts
@@ -1,11 +1,16 @@
+import { getRepository } from "typeorm";
 import { AuthorEntity } from "../../database/entities/AuthorEntity";
 
-export const getPersonalInfo = function(json) {
+export const getPersonalInfo = async function(json) {
     let author = new AuthorEntity();
 
     author.name = json['CURRICULO-VITAE']['DADOS-GERAIS']['@_NOME-COMPLETO']
     author.orcid = json['CURRICULO-VITAE']['DADOS-GERAIS']['@_ORCID-ID']
     author.institution = json['CURRICULO-VITAE']['DADOS-GERAIS']['ENDERECO']['ENDERECO-PROFISSIONAL']['@_NOME-INSTITUICAO-EMPRESA']
-    
-    return json['CURRICULO-VITAE']['DADOS-GERAIS']['@_NOME-COMPLETO']
+    author.lattesId = json['CURRICULO-VITAE']['@_NUMERO-IDENTIFICADOR']
+
+    author = await getRepository(AuthorEntity).save(author);
+    console.log(author.name);
+
+    return author.name
 }
