204d078f73cc60d95cb1ea3d896fe212b4339ac2
als-v <alisson.v3@hotmail.com>
2022-05-11 17:26:27 -0300

    fix: 🐛 Fix lines Author

--- src/services/author/author.ts
+++ src/services/author/author.ts
@@ -4,19 +4,12 @@ import { AuthorEntity } from "../../database/entities/AuthorEntity";
 export const getPersonalInfo = async function(json) {
     const AuthorRepository = getRepository(AuthorEntity);
 
-    const orcId = json['CURRICULO-VITAE']['DADOS-GERAIS']['@_ORCID-ID']
-    const lattesId = json['CURRICULO-VITAE']['@_NUMERO-IDENTIFICADOR']
-
-    if (orcId){
-        // AuthorRepository.find
-    }
-
     let author = new AuthorEntity();
 
     author.name = json['CURRICULO-VITAE']['DADOS-GERAIS']['@_NOME-COMPLETO']
-    author.orcid = orcId
+    author.orcid = json['CURRICULO-VITAE']['DADOS-GERAIS']['@_ORCID-ID']
     author.institution = json['CURRICULO-VITAE']['DADOS-GERAIS']['ENDERECO']['ENDERECO-PROFISSIONAL']['@_NOME-INSTITUICAO-EMPRESA']
-    author.lattesId = lattesId
+    author.lattesId = json['CURRICULO-VITAE']['@_NUMERO-IDENTIFICADOR']
 
     author = await AuthorRepository.save(author);
 
