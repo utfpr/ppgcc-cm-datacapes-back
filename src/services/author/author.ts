632bae41838f1a936fa2e5f8b1a983e1d7f28c4a
als-v <alisson.v3@hotmail.com>
2022-06-01 16:50:22 -0300

    refactor: 💡 Changes in methods
    Methods: Create Author and create Citations

--- src/services/author/author.ts
+++ src/services/author/author.ts
@@ -5,9 +5,10 @@ export const getAuthor = async function(author: AuthorEntity) {
     const AuthorRepository = getRepository(AuthorEntity);
 
     let authorFound = await AuthorRepository.findOne({ lattesId: author.lattesId });
-    
+
     if (!authorFound) {
         authorFound = AuthorRepository.create(author);
+        
     } else {
         authorFound.name = author.name;
         authorFound.orcid = author.orcid;
--- src/services/citations/citations.ts
+++ src/services/citations/citations.ts
@@ -1,22 +1,32 @@
 import { AuthorEntity } from "../../database/entities/AuthorEntity";
 import { CitationNameEntity } from "../../database/entities/CitationNameEntity";
-import { getRepository } from "typeorm";
+import { getManager, getRepository } from "typeorm";
 
-export const getCitations = function(citationNames: string[], author: AuthorEntity) {
+export const getCitations = async function(citationNames: string[], author: AuthorEntity) {
     const citationNameRepository = getRepository(CitationNameEntity);
     
     let citations: Array<CitationNameEntity> = []
 
-    citationNames.forEach(async (name) => {
-        let citationFound = await citationNameRepository.findOne({ author: author, name: name});
+    for (const name of citationNames) {
+        
+        let citationFound = await citationNameRepository.findOne({
+            where: { name: name, author: author },
+            relations: ['author']
+        });
 
-        let citation = new CitationNameEntity();
-        citation.name = name;
-        citation.author = author;
 
-        citations.push(citationNameRepository.create(citation));
-    });
+        if (undefined != citationFound){
+            let citation = new CitationNameEntity();
+            citation.name = name;
+            citation.author = author;
+    
+            citations.push(citationNameRepository.create(citation));
 
-    console.log('Citations: ', citations);
+        } else {
+            citations.push(citationFound);
+        }
+
+    }
+
     return citations;
 }
